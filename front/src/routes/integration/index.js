77f07f8c2e6edafe36cb5563ad1e61ea4dd2c8dc
bertrandda <bertrandda@users.noreply.github.com>
2026-04-06 00:13:00 +0200

    Complete debounce rollback

--- front/src/routes/integration/index.js
+++ front/src/routes/integration/index.js
@@ -21,7 +21,7 @@ class Integration extends Component {
       searchKeyword: '',
       orderDir: 'asc'
     };
-    this.getIntegrationsDebounced = debounce(this.getIntegrations, 300).bind(this);
+    this.getIntegrationsDebounced = debounce(this.getIntegrations, 300);
   }
 
   componentWillMount() {
